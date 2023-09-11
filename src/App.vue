04f4b901471cab063ba1f753adb1c29286a93b6c
Augusto <ampiatto@hotmail.com>
2023-09-11 06:27:29 -0300

    savepoint

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@
       <MyCheckbox v-model="checkboxValue" value="Standard">Standard</MyCheckbox>
       <MyCheckbox v-model="checkboxValue" value="Modern">Modern</MyCheckbox>
       <MyCheckbox v-model="checkboxValue" value="Pauper">Pauper</MyCheckbox> -->
+      <!-- step 2
       <MyRadio v-model="radioValue" :items="radioItems"
         >Você sabe o que é Circuito LigaMagic?</MyRadio
-      >
+      > -->
+      <MyTextField></MyTextField>
     </template>
   </MyStepper>
 </template>
@@ -16,12 +18,14 @@
 import MyCheckbox from "./components/MyCheckbox.vue";
 import MyRadio from "./components/MyRadio.vue";
 import MyStepper from "./components/MyStepper.vue";
+import MyTextField from "./components/MyTextField.vue";
 
 export default {
   components: {
     MyCheckbox,
     MyStepper,
     MyRadio,
+    MyTextField,
   },
   data() {
     return {
@@ -32,11 +36,6 @@ export default {
       ],
       radioValue: "",
     };
-  },
-  watch: {
-    radioValue() {
-      console.log(this.radioValue);
-    },
   },
 };
 </script>
