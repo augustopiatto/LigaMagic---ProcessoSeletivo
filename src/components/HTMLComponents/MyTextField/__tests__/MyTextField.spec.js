4616ebaa4efdc6c5402bbb52f289a17755514cb8
Augusto <ampiatto@hotmail.com>
2023-09-14 02:54:13 -0300

    teste de textfield

--- src/components/HTMLComponents/MyTextField/__tests__/MyTextField.spec.js
+++ src/components/HTMLComponents/MyTextField/__tests__/MyTextField.spec.js
@@ -5,7 +5,17 @@ import MyTextField from "../MyTextField.vue";
 describe("MyTextField.vue", () => {
   test("Componente sendo montado", () => {
     const wrapper = mount(MyTextField, {});
+    const textInput = wrapper.find("input");
 
-    // expect(wrapper.getAttribute("checked")).toBe(false);
+    expect(textInput.element.value).toBe("");
+  });
+
+  test("Texto digitado", async () => {
+    const wrapper = mount(MyTextField, {});
+    const textInput = wrapper.find("input");
+
+    await textInput.setValue("Tomas");
+
+    expect(textInput.element.value).toBe("Tomas");
   });
 });
