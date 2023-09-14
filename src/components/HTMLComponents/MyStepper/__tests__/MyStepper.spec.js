6f98da87b0d248599ec070352240c146c7129bc5
Augusto <ampiatto@hotmail.com>
2023-09-14 04:56:35 -0300

    teste mystepper

--- src/components/HTMLComponents/MyStepper/__tests__/MyStepper.spec.js
+++ src/components/HTMLComponents/MyStepper/__tests__/MyStepper.spec.js
@@ -5,7 +5,32 @@ import MyStepper from "../MyStepper.vue";
 describe("MyStepper.vue", () => {
   test("Componente sendo montado", () => {
     const wrapper = mount(MyStepper, {});
+    const submitButtons = wrapper.findAll("button");
+    const steps = wrapper.findAll(".sh__step--whole");
 
-    // expect(wrapper.getAttribute("checked")).toBe(false);
+    expect(submitButtons.length).toBe(2);
+    expect(submitButtons[1].text()).toBe("Avançar");
+    expect(steps.length).toBe(3);
+  });
+
+  test("Componente com props secondary", () => {
+    const wrapper = mount(MyStepper, {
+      propsData: {
+        secondary: true,
+      },
+    });
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(["secondary"]));
+  });
+
+  test("Componente no último passo tem botão de enviar", () => {
+    const wrapper = mount(MyStepper, {
+      propsData: {
+        currentStep: 2,
+      },
+    });
+    const submitButtons = wrapper.findAll("button");
+
+    expect(submitButtons[1].text()).toBe("Enviar");
   });
 });
